8936428e19e8ae6cf538f68b7d88ca66b6429947
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-07-15 19:39:28 -0300

    Feat: A beautifful outline for inputs is created

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,9 +4,9 @@ export const UserInput = styled.input `
 
     padding: 0.75rem;
     width: ${ ({ type }) => type !== "number" ? "100%" : "50%" };
-    border: 1px solid #440445;
+    border: 1px solid rgba(93, 17, 95, 0.64);
+    outline-color: #440445;
     border-radius: 0.3rem;
-    outline: none;
     color: #440445;
     font-size: large;
     font-weight: 500;
